f445e8d1d6517766896ff5e83d6573be2806d08f
Zaki Samson <zakisamson@gmail.com>
2023-03-28 15:05:53 +0700

    added assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world
-      </h1>
-    </div>
+    <main className="App">
+      
+    </main>
   )
 }
 
